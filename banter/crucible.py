378ff44fb257f6111713aaa3ea3c6e967d5f801c
Luke Plant <L.Plant.98@cantab.net>
2015-09-22 16:19:40 +0100

    Don't use @staticmethod in Crucible class
    This hinders re-use by subclassing

--- banter/crucible.py
+++ banter/crucible.py
@@ -4,6 +4,7 @@ import requests
 
 from . import dict2xml, utils
 
+
 class Crucible(object):
     def __init__(self, baseurl):
         self.baseurl = baseurl
@@ -16,8 +17,7 @@ class Crucible(object):
             return None
         return response.json()['token']
 
-    @staticmethod
-    def get_auth_token_request(username, password):
+    def get_auth_token_request(self, username, password):
         return {
             'method': 'GET',
             'url': '/rest-service/auth-v1/login',
@@ -32,19 +32,17 @@ class Crucible(object):
         request['data'] = self.prepare_xml_payload(request['data'])
         return self.make_request(request)
 
-    @staticmethod
-    def get_create_review_request(token, **kwargs):
+    def get_create_review_request(self, token, **kwargs):
         return {
             'method': 'POST',
             'url': '/rest-service/reviews-v1',
             'params': {
                 'FEAUTH': token
             },
-            'data': Crucible.get_create_review_payload(**kwargs)
+            'data': self.get_create_review_payload(**kwargs)
         }
 
-    @staticmethod
-    def get_create_review_payload(**kwargs):
+    def get_create_review_payload(self, **kwargs):
         reviewData = {
             'allowReviewersToJoin': kwargs['allow_reviewers_to_join'],
             'author': {'userName': kwargs['author']},
@@ -68,8 +66,7 @@ class Crucible(object):
         request = self.get_add_reviewers_request(token, review_id, reviewers)
         return self.make_request(request)
 
-    @staticmethod
-    def get_add_reviewers_request(token, review_id, reviewers):
+    def get_add_reviewers_request(self, token, review_id, reviewers):
         return {
             'method': 'POST',
             'url': '/rest-service/reviews-v1/' + review_id + '/reviewers',
@@ -79,14 +76,12 @@ class Crucible(object):
             'data': ','.join(reviewers)
         }
 
-    @staticmethod
-    def prepare_xml_payload(data):
+    def prepare_xml_payload(self, data):
         result = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
         result += dict2xml.dict2xml(data)
         return result
 
-    @staticmethod
-    def get_headers():
+    def get_headers(self):
         return {
             'content-type': 'application/xml',
             'accept': 'application/json'
